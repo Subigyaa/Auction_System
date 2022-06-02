2d2c2a39749419d8121728e598ae944860bcb7fd
Subigya Pyakurel <subigya.pyakurel@apexcollege.edu.np>
2022-06-02 19:59:46 +0545

    modules

--- auction_system/product_module/admin.py
+++ auction_system/product_module/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Category, Product
+from .models import Category, Product, Customer, Vendor, Bids
 admin.site.register(Category)
 admin.site.register(Product)
-admin.site.register(Brand)
+admin.site.register(Customer)
+admin.site.register(Vendor)
+admin.site.register(Bids)
 
 
--- auction_system/product_module/models.py
+++ auction_system/product_module/models.py
@@ -19,4 +19,31 @@ class Product(models.Model):
     condition = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    
+class Customer(models.Model):
+    first_name= models.CharField(max_length=200)
+    last_name= models.CharField(max_length=200)
+    mobile_number= models.IntegerField(max_length=10)
+    address= models.CharField(max_length=200)
+    email= models.CharField(max_length=200)
+    username= models.CharField(max_length=200)
+    password= models.CharField(max_length=200)
+
+class Vendor(models.Model):
+    first_name= models.CharField(max_length=200)
+    last_name= models.CharField(max_length=200)
+    mobile_number= models.IntegerField(max_length=10)
+    address= models.CharField(max_length=200)
+    email= models.CharField(max_length=200)
+    username= models.CharField(max_length=200)
+    password= models.CharField(max_length=200)
+    shop_name= models.CharField(max_length=200)
+
+class Bids(models.Model):
+    bid_amount= models.IntegerField(max_length=100)
+    bid_time= models.DateTimeField()
+    status_choices = (('Sold', 'Sold'),
+        ('On Bid', 'On Bid'), 
+        )
+    status = models.CharField(max_length = 100, choices = status_choices, default="In Progress")
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
